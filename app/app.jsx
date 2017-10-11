d1831bb4e8e9899619f7d1918a5f5ae22b9138cc
Davy'd <pdaswift3009@gmail.com>
2017-10-11 21:32:01 +0100

    render index route

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,13 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory } = require('react-router');
 var Main = require('Main');
-
+var Weather = require('Weather');
 
 
 ReactDOM.render(
       <Router history={hashHistory}>
           <Route path='/' component={Main}>
-          
+             <IndexRoute component={Weather}/>  
           </Route>    
       </Router>,
       document.getElementById('app')
